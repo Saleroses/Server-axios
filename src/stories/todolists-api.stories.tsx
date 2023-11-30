5743474ab47c140147fc39603cb5c8e36dfc91ee
Egor <saleroses@yandex.ru>
2023-11-30 21:07:05 +0300

    2

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react'
+import axios from "axios";
 
 export default {
     title: 'API'
@@ -9,6 +10,7 @@ export const GetTodolists = () => {
     useEffect(() => {
         // здесь мы будем делать запрос и ответ закидывать в стейт.
         // который в виде строки будем отображать в div-ке
+        axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists')
 
     }, [])
     return <div>{JSON.stringify(state)}</div>
