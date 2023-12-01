936be25986bf957010eb9ff8f9990889a798b9b4
Egor <saleroses@yandex.ru>
2023-12-01 20:16:53 +0300

    axios

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -5,12 +5,16 @@ export default {
     title: 'API'
 }
 
+const config = {withCredentials: true} // переменная с доп параметрами запроса
+
 export const GetTodolists = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-        // здесь мы будем делать запрос и ответ закидывать в стейт.
-        // который в виде строки будем отображать в div-ке
-        axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists')
+
+       const promise = axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', config)
+           promise.then((res)=> {
+               setState(res.data)
+           })
 
     }, [])
     return <div>{JSON.stringify(state)}</div>
@@ -18,13 +22,22 @@ export const GetTodolists = () => {
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-    }, [])
+        axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists',{title: 'First'}, config)
+            .then((res)=>{
+                setState(res.data)
+            })
+        }, [])
 
     return <div>{JSON.stringify(state)}</div>
 }
 export const DeleteTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        const todoId = '15d3435f-7419-4f08-b1ef-5f6d845ebf8e'
+        axios.delete(`https://social-network.samuraijs.com/api/1.1//todo-lists/${todoId}`, config)
+            .then((res)=>{
+                setState(res.data)
+            })
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
@@ -32,6 +45,11 @@ export const DeleteTodolist = () => {
 export const UpdateTodolistTitle = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        const todoId = '1d660dd6-0bd1-46ba-a503-47533c82e11d'
+        axios.put(`https://social-network.samuraijs.com/api/1.1//todo-lists/${todoId}`, {title: 'React'},config)
+            .then((res)=>{
+                setState(res.data)
+            })
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
